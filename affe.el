cedda5b23e7a6d3ece6f034797cd1813c99dc3d2
kohnish <kohnish@gmx.com>
2024-10-30 23:40:43 +0100

    Fix wrong deps

--- affe.el
+++ affe.el
@@ -7,7 +7,6 @@
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;; Created: 2021
 ;; Version: 0.8
-;; Package-Requires: ((emacs "28.1") (consult "1.7"))
 ;; URL: https://github.com/minad/affe
 ;; Keywords: matching, files, completion
 
